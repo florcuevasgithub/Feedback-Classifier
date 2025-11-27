818ef2f4c4eb8d17160f9df3194d70db467df1d0
Leandro74377 <leandrobaque4@gmail.com>
2025-11-27 15:08:11 -0500

    Initialize models module with imports

---
+++ Feedback-Classifier/app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.config.db import Base
+from app.models.app_user import AppUser
+from app.models.feedback import Feedback
+from app.models.feedback_source import FeedbackSource
+from app.models.feedback_topic import FeedbackTopic
+from app.models.export_job import ExportJob
